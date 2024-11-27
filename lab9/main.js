14eb07cb1f300b67cc62e399bca67fe8af35d3c9
Lucía <lucia.manso@live.u-tad.com>
2024-11-27 20:28:17 +0000

    Lab 9 - CSS but better

--- lab9/main.js
+++ lab9/main.js
@@ -20,14 +20,20 @@ function criarProduto(produto) {
     const newArticle = document.createElement("article");
     newArticle.className = "product"
 
+    // Título
     const title = document.createElement("h3")
     title.textContent = produto.title;
+    // Image
     const image = document.createElement("img");
     image.src = produto.image;
     image.alt = produto.title;
+    // Price
     const price = document.createElement("p")
+    price.className = "product-price";
     price.textContent = `Precio: ${produto.price}$`;
+    // Description
     const description = document.createElement("p")
+    description.className = "product-description";
     description.textContent = produto.description;
 
     newArticle.append(title, image, price, description);
